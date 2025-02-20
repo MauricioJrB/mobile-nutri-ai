c9ea71ab36d36f41ff909bbd26a1b83842c0c6cf
MauricioJrB <mjunior1205@gmail.com>
2025-02-20 19:25:03 -0300

    refactor(routes): renamed Register to SignUp in router

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,10 +3,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { RootStackParamList } from "./types/RootStackParamList";
 
-import Home from "./screens/Home";
 import Initial from "./screens/Initial";
-import Register from "./screens/register";
 import Login from "./screens/login";
+import SignUp from "./screens/SignUp";
+//import Home from "./screens/Home";
 
 const Stack = createStackNavigator<RootStackParamList>();
 
@@ -22,14 +22,6 @@ const Routes = () => {
           }}
         />
         <Stack.Screen
-          name="Register"
-          component={Register}
-          options={{
-            title: "Register",
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
           name="Login"
           component={Login}
           options={{
@@ -38,12 +30,20 @@ const Routes = () => {
           }}
         />
         <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{
+            title: "SignUp",
+            headerShown: false,
+          }}
+        />
+        {/* <Stack.Screen
           name="Home"
           component={Home}
           options={{
             headerShown: false,
           }}
-        />
+        /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
