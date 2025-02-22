61e5e7b64096503ee405e2be4f803eb4cd06da30
MauricioJrB <mjunior1205@gmail.com>
2025-02-22 16:16:31 -0300

    feat(app): added use fonts with KumbhSans

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import Routes from "./src/routes";
 import React from "react";
 import { ThemeProvider } from "styled-components/native";
-import colorTheme from './src/styles/themes/index';
+import colorTheme from "./src/styles/themes/index";
 import { useColorScheme } from "react-native";
+import {
+  useFonts,
+  KumbhSans_300Light,
+  KumbhSans_400Regular,
+  KumbhSans_500Medium,
+  KumbhSans_600SemiBold,
+  KumbhSans_700Bold,
+} from "@expo-google-fonts/kumbh-sans";
 
 const App = () => {
+  useFonts({
+    KumbhSans_300Light,
+    KumbhSans_400Regular,
+    KumbhSans_500Medium,
+    KumbhSans_600SemiBold,
+    KumbhSans_700Bold,
+  });
 
   const colorScheme = useColorScheme();
   const theme = colorTheme[colorScheme ? colorScheme : "dark"];
