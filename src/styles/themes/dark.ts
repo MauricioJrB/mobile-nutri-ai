9ce3cd3582a9d740f42b99ee6a864880ac9e0b1e
MauricioJrB <mjunior1205@gmail.com>
2025-02-25 17:14:17 -0300

    style(dark): changed colors in dark theme

--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -4,38 +4,42 @@ import { fonts } from "./fonts";
 export const dark: DefaultTheme = {
   colors: {
     screen: {
-      background: "#0A0A0A",
+      background: "#1E1E1E",
     },
     header: {
-      background: "#F0F0F0",
-      title: "#000000",
+      background: "#D8FF0B",
+      title: "#1E1E1E",
     },
     input: {
       background: {
-        primary: "#F0F0F0",
+        primary: "#1E1E1E",
         secondary:
-          "linear-gradient(to bottom, rgba(10, 10, 10, 0.75) 0%, #707070 100%",
+          "linear-gradient(to bottom, rgba(10, 10, 10, 0.75) 0%, #D8FF0B 100%",
       },
       label: {
-        color: "#E1FF41",
+        color: "#D8FF0B",
         size: 20,
       },
       placeholder: {
-        primary: "#303030",
-        secondary: "#3E1FF41",
+        primary: "#FFFFFF",
+        secondary: "#D8FF0B",
         size: 18,
+        icon: {
+          primary: "#FFFFFF",
+          secondary: "#D8FF0B",
+        },
       },
-      width: 200,
+      width: 274,
       height: 54,
     },
     button: {
-      background: "#E1FF41",
-      color: "#000000",
+      background: "#D8FF0B",
+      color: "#1E1E1E",
       font: fonts.bold,
       size: 25,
       width: 274,
       height: 54,
     },
   },
-  fonts: fonts
+  fonts: fonts,
 };
