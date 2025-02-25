00140fdc8bc5d677c63cbb6188d5a4875f96fcdd
MauricioJrB <mjunior1205@gmail.com>
2025-02-25 17:13:55 -0300

    style(light): changed colors in light theme

--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -4,32 +4,36 @@ import { fonts } from "./fonts";
 export const light: DefaultTheme = {
   colors: {
     screen: {
-      background: "#FFFFFF",
+      background: "#E6E6E6",
     },
     header: {
-      background: "#F0F0F0",
+      background: "#9E9E9E",
       title: "#303030",
     },
     input: {
       background: {
         primary: "#F0F0F0",
         secondary:
-          "linear-gradient(to bottom, rgba(10, 10, 10, 0.75) 0%, #707070 100%",
+          "linear-gradient(to bottom, rgba(10, 10, 10, 0.75) 0%, #D8FF0B 100%",
       },
       label: {
-        color: "#E1FF41",
+        color: "#D8FF0B",
         size: 20,
       },
       placeholder: {
         primary: "#303030",
-        secondary: "3E1FF41",
+        secondary: "#D8FF0B",
         size: 18,
+        icon: {
+          primary: "#FFFFFF",
+          secondary: "#D8FF0B",
+        },
       },
       width: 274,
       height: 54,
     },
     button: {
-      background: "#E1FF41",
+      background: "#D8FF0B",
       color: "#000000",
       font: fonts.bold,
       size: 25,
@@ -37,5 +41,5 @@ export const light: DefaultTheme = {
       height: 54,
     },
   },
-  fonts: fonts
+  fonts: fonts,
 };
