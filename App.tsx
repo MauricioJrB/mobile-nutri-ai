9cac3e58abce467f9055a1d3dc4fe3043a727f5e
MauricioJrB <mjunior1205@gmail.com>
2025-02-28 22:08:22 -0300

    (feat(App): added color scheme theme example

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import Routes from "./src/routes";
-import React from "react";
+import Router from "./src/routes/Router";
+import React, { useState } from "react";
 import { ThemeProvider } from "styled-components/native";
 import colorTheme from "./src/styles/themes/index";
-import { useColorScheme } from "react-native";
+import { TouchableOpacity, useColorScheme } from "react-native";
+
 import {
   useFonts,
   KumbhSans_300Light,
@@ -11,6 +12,7 @@ import {
   KumbhSans_600SemiBold,
   KumbhSans_700Bold,
 } from "@expo-google-fonts/kumbh-sans";
+import { AuthProvider } from "./src/contexts/Auth";
 
 const App = () => {
   useFonts({
@@ -21,13 +23,20 @@ const App = () => {
     KumbhSans_700Bold,
   });
 
-  const colorScheme = useColorScheme();
-  const theme = colorTheme[colorScheme ? colorScheme : "dark"];
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
+  const theme = isDarkTheme ? colorTheme.dark : colorTheme.light;
+
+  // example
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Routes />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
+    </AuthProvider>
   );
 };
 
